a635ed915cb4537e2073eba5cbcd02a546699431
michaltaratuta <m.taratuta@hotmail.com>
2017-07-07 11:38:04 +0100

    cisco_asa_acl with logic -- WORKING TODO: Save the running conf to start up conf

--- cisco_asa_acl.py
+++ cisco_asa_acl.py
@@ -5,37 +5,41 @@ from netmiko import ConnectHandler
 
 def execute_acl(data):
     params = data.copy()
-    del params['lines']
+    del params['lists']
 
     has_changed = 0
     has_not_changed = 0
 
     connect = ConnectHandler(**params)
-    acl_config = connect.send_connect('show running-config access-list')
+    acl_config = connect.send_command('show running-config access-list')
     connect.config_mode()
 
-    for acl in data['lines']:
+    for acl in data['lists']:
         if acl not in acl_config:
-            connect.send_connect(acl)
+            command = connect.send_command(acl)
+            regex = re.search('(error|warning)', command, re.IGNORECASE)
+            if regex:
+                return True, True, {"status": command}
             has_changed += 1
         else:
             has_not_changed += 1
 
     if has_changed > 0:
         return False, True, {"status": "Has changed some ASL(s)"}
-    elif (has_not_changed > 0 and has_not_changed == len(data['lines'])
+    elif (has_not_changed > 0 and has_not_changed == len(data['lists'])
             and has_changed == 0):
         return False, False, {"status": "ACL(s) already exist"}
     else:
-        return True, True, {"status": "Has changed some ASL(s)"}
+        return True, True, {"status": "Something went really wrong" +
+               " " + "you should have never see this message"}
 
 def main():
     fields = {
         "device_type": {"default": "cisco_asa", "type": "str"},
         "ip": {"required": True, "type": "str"},
         "username": {"required": True, "type": "str"},
-        "password": {"required": True, "type": "str"},
-        "secret": {"required": False, "type": "str"},
+        "password": {"required": True, "type": "str", "no_log": True},
+        "secret": {"required": False, "type": "str", "no_log": True},
         "lists": {"required": True, "type": "list"}
     }
 
